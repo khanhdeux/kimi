e67ce7fd838eca5d511bc81344dac804e7319c44
Quoc Khanh Nguyen <khanh.nguyen@arrabiata.de>
2016-10-04 11:35:24 +0200

    Custom price page

---
+++ wp-content/themes/skt-cutsnstyle-lite/page-price.php
@@ -0,0 +1,25 @@
+<?php /* Template Name: PricePage */ ?>
+<?php get_header(); ?>
+
+	<div class="container">
+      <div class="page_content">
+    		 <section class="site-main">               
+            		<?php if( have_posts() ) :
+							while( have_posts() ) : the_post(); ?>
+                                <div class="entry-content">
+                                			<?php the_content(); ?>
+                                            <?php
+												//If comments are open or we have at least one comment, load up the comment template
+												if ( comments_open() || '0' != get_comments_number() )
+													comments_template();
+												?>
+                                </div><!-- entry-content -->
+                      		<?php endwhile; else : endif; ?>
+                    
+            </section><!-- section-->
+   
+     <?php get_sidebar();?>      
+    <div class="clear"></div>
+    </div><!-- .page_content --> 
+ </div><!-- .container --> 
+<?php get_footer(); ?>
